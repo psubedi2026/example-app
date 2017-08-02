246ca00f0daefd246fd9da2f41557d79eb810b85
Prakash Subedi <psubedi2026@Prakashs-MBP.lan>
2017-08-02 10:16:52 -0400

    added couple of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { FormsModule } from '@angular/forms';
+import { AnotherComponentComponent } from './another-component/another-component.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent,
+    AnotherComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
